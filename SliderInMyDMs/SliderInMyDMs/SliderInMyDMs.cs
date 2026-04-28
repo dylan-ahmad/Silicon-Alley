15d3b8b9b033fe935b7e3b95d996d5c33d2d9d71
LuckyLuke00 <lucas.kinoo5@gmail.com>
2026-04-28 15:20:18 +0200

    Implement options example mod

---
+++ SliderInMyDMs/SliderInMyDMs/SliderInMyDMs.cs
@@ -0,0 +1,24 @@
+﻿using BAModAPI;
+using SliderInMyDMs.Logic;
+
+[assembly: RegisterModClass(typeof(OptionsMod))]
+
+[ModEntryMainMenu]
+public class OptionsMod : IModBigAmbitions
+{
+    private readonly OptionsLogic _logic = new();
+
+    public string[] RelativeAssetBundlePaths => Array.Empty<string>();
+
+    public Task OnLoadAsync(ModContext context)
+    {
+        _logic.Initialize(context);
+        return Task.CompletedTask;
+    }
+
+    public Task OnUnloadAsync()
+    {
+        _logic.Shutdown();
+        return Task.CompletedTask;
+    }
+}
